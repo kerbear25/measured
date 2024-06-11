9eb67d25c6e11b9f5a34919e3f1b0d1f49e49c80
Keriann <keri.gannon@pm.me>
2024-06-11 17:16:50 -0500

    update text on button to submit

--- src/app/components/EmailInput/EmailInput.tsx
+++ src/app/components/EmailInput/EmailInput.tsx
@@ -66,7 +66,7 @@ export default function EmailInput() {
           disabled={hasValidEmail !== true}
           onClick={handleSubmit}
         >
-          Next
+          Submit
         </button>
       </div>
     </>
